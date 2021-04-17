f2e1306a367c95af4aa723fd039a4be5e9c3c448
D19127919 <77803253+D19127919@users.noreply.github.com>
2021-04-17 07:25:32 -0700

    Initial commit
    Includes functioning prefabs that hopefully don't have the AI patrol points on the bullet layer, and the initial tutorial area.

---
+++ GameToolsVision/Assets/Scripts/Player_Movement.cs
@@ -0,0 +1,91 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class Player_Movement : MonoBehaviour
+{
+    [SerializeField] private float moveSpeed, maxSpeed, jumpForce;
+    [SerializeField] private Collider2D groundCheck;
+    [SerializeField] private LayerMask groundLayers;
+    [SerializeField] private bool cancelJumpEnabled;
+    public int MaxJump = 1;
+    private int JumpCount = 1;
+    
+
+    private float moveDir;
+    private Rigidbody2D myRB;
+    private bool canJump;
+    private SpriteRenderer mySprite;
+
+    private void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        mySprite = GetComponentInChildren<SpriteRenderer>();
+    }
+
+    private void FixedUpdate()
+    {
+        if (moveDir > 0)
+        {
+            mySprite.flipX = false;
+        }
+
+        if (moveDir < 0)
+        {
+            mySprite.flipX = true;
+        }
+        var moveAxis = Vector2.right * moveDir;
+
+        if (Mathf.Abs(myRB.velocity.x) < maxSpeed)
+        {
+            myRB.AddForce(moveAxis * moveSpeed, ForceMode2D.Force);
+        }
+
+        if (groundCheck.IsTouchingLayers(groundLayers))
+        {
+            JumpCount = MaxJump;
+
+        }
+
+        if (JumpCount > 0)
+        {
+            canJump = true;
+        }
+        else if (JumpCount < 1)
+        {
+            canJump = false;
+        }
+
+    }
+
+    public void Move(InputAction.CallbackContext context)
+    {
+        moveDir = context.ReadValue<float>();
+    }
+
+    public void Move(float moveAmt)
+    {
+        moveDir = moveAmt;
+    }
+
+    public void Jump(InputAction.CallbackContext context)
+    {
+        if (canJump)
+        {
+            if (context.started)
+            {
+                myRB.AddForce(Vector3.up * jumpForce, ForceMode2D.Impulse);
+                canJump = false;
+                
+            }
+            JumpCount--;
+        }
+
+        if (context.canceled && cancelJumpEnabled)
+        {
+            myRB.velocity = new Vector2(myRB.velocity.x, 0f);
+        }
+    }
+}
